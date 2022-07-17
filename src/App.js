696cce0bc561f9c48c79a5956def95b8b8739828
shor-gad-github <shorgad@outlook.com>
2022-07-17 16:59:21 +0300

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './Layout';
+import Home from './Home';
+import Blogs from './Blogs';
+import Contact from './Contact';
+import NoPage from './NoPage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <h1>Demo</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout></Layout>}>
+            <Route index element={<Home ></Home>} />
+            <Route path="blogs" element={<Blogs ></Blogs>} />
+            <Route path="contact" element={<Contact ></Contact>} />
+            <Route path="*" element={<NoPage ></NoPage>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
